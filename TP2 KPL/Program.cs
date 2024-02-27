92504e3c0ed5d1e9d0604e2c2d7ebe6dba1b55a7
spacyes241 <135798550+spacyes241@users.noreply.github.com>
2024-02-27 14:48:30 +0700

    Test

---
+++ TP2 KPL/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//// contoh output 
+//Console.WriteLine("Enter Username:");
+
+//// Contoh Input 
+//string Username = Console.ReadLine();
+
+//// menghitung berapa banyak kata dari inputan
+//Console.WriteLine("The lenghth of input string is: " + Username.Length);
+
+//// contoh output dari inputan user 
+//Console.WriteLine("The Username is :" + Username);
+
+
+//soal 1 tp mod 2 
+//Console.WriteLine("Masukan Satu Huruf: ");
+
+//String input = Console.ReadLine();
+
+//if ((input == "A") || (input == "I") || (input == "U") || (input == "E") || (input == "O") || (input == "a") || (input == "i") || (input == "u") || (input == "e") || (input == "o"))
+//{
+//    Console.WriteLine("Huruf" + " " + input + " " + "merupakan huruf vokal");
+//}
+//else
+//{
+//    Console.WriteLine("Huruf" + " " + input + " " + "merupakan huruf konsonan");
+//}
+
+
+//soal 2 tp mod 2 
+
+//int temp = 1;
+//int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+
+//for (int i = 0; i < bilanganGenap.Length; i++)
+//{
+//    Console.WriteLine("Angka Genap" + " " + temp + ":" + bilanganGenap[i]);
+//    temp++;
+//}
+
+
